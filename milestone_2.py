85360d41da614e27be01889cdd9261ccb7f7bb19
JoeDonnelly92 <148227221+JoeDonnelly92@users.noreply.github.com>
2023-12-02 10:46:19 +0000

    Creates milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,2 +1,21 @@
+import random
+
 word_list = ["Pineapple","Mango","Passionfruit","Grape","Banana"]
-print(word_list)
+word = random.choice(word_list)
+
+def get_guess():
+    while True:
+        guess = input("Enter a letter: ")
+        if len(guess) == 1 and guess.isalpha() == True:
+            print("Nice length")
+            break
+        else:
+            print("Oops! That is not a valid input")
+    return
+
+def main():
+    guess = get_guess()
+    print(guess)
+
+main()
+
