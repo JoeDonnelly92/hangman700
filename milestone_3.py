c139e73f8e59bcea2990ab4acbaa800336b393dd
JoeDonnelly92 <148227221+JoeDonnelly92@users.noreply.github.com>
2023-12-02 12:18:55 +0000

    Builds functions to complete actions in game

--- milestone_3.py
+++ milestone_3.py
@@ -3,13 +3,29 @@ import random
 word_list = ["Pineapple","Mango","Passionfruit","Grape","Banana"]
 word = random.choice(word_list)
 
-def get_guess:
-while True:
-    guess = input("Enter a letter: ")
-    if guess.isalpha() == True and len(guess) ==1:
-        print("Good guess!")
-        break
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
-return guess 
 
+def ask_for_input(): 
+    guess = input("Enter a letter: ")
+    while True:
+        if guess.isalpha() == True and len(guess) ==1:
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+            guess = input("Enter a letter: ")
+            continue
+    return guess
+
+def check_guess(guess):
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+
+def main():
+    guess = ask_for_input()
+    check_guess(guess)
+
+
+if __name__ == "__main__":
+    main()
