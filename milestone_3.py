2206d2cc6026900287a4f83a790f72fd1102c6a6
JoeDonnelly92 <148227221+JoeDonnelly92@users.noreply.github.com>
2023-12-02 11:41:56 +0000

    Add code from previous file to milestone_3.

--- milestone_3.py
+++ milestone_3.py
@@ -1,3 +1,9 @@
+import random
+
+word_list = ["Pineapple","Mango","Passionfruit","Grape","Banana"]
+word = random.choice(word_list)
+
+def get_guess:
 while True:
     guess = input("Enter a letter: ")
     if guess.isalpha() == True and len(guess) ==1:
@@ -5,3 +11,5 @@ while True:
         break
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
+return guess 
+
