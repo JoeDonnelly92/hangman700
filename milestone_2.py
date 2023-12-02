57da5b3a98dac4358b8a67d92ca8453e1b4c7914
JoeDonnelly92 <148227221+JoeDonnelly92@users.noreply.github.com>
2023-12-02 11:34:30 +0000

    Implements guess checking mechanism

--- milestone_2.py
+++ milestone_2.py
@@ -4,18 +4,18 @@ word_list = ["Pineapple","Mango","Passionfruit","Grape","Banana"]
 word = random.choice(word_list)
 
 def get_guess():
+    guess = input("Enter a letter: ")
     while True:
-        guess = input("Enter a letter: ")
         if len(guess) == 1 and guess.isalpha() == True:
             print("Nice length")
             break
         else:
             print("Oops! That is not a valid input")
-    return
+            break
+    return guess
 
 def main():
     guess = get_guess()
-    print(guess)
 
 main()
 
