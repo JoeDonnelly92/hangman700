795039d9cb59a42f4456a241c7081810de621045
JoeDonnelly92 <148227221+JoeDonnelly92@users.noreply.github.com>
2023-12-03 00:24:40 +0000

    formatting

--- milestone_5.py
+++ milestone_5.py
@@ -23,19 +23,27 @@ class Hangman:
         #Checks if guessed letter is present in randomly selected word.
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
-            #intiialise variable to act as stand-in for position of guessed letter in word.
+            #enumerates over chars in word, if any match guess it reveals them in variable 'word_guessed'
             for i,x in enumerate(self.word):
                 if guess == x:
                     self.word_guessed[i] = guess
-                 
+             #lowers number of remaining letters by 1    
             self.num_letters -=1
+            #prints word with correctly guessed letters revealed and all others as underscores.
             print(self.word_guessed)
         else:
+            #If letter not present in word, num_lives lowered by 1 and user prompted for input
             self.num_lives -=1
             print(f"Sorry, {guess} is not in the word. Try again.")
             print(f"You have {self.num_lives} left.")
 
     def ask_for_input(self): 
+        '''The method "ask_for_input" prompts a user for a guess in the form of a single letter.
+            The method validates this input to ensure it is a single, alphabetical character and converts it to lowercase. \n
+            If the guess is not a valid input, the method continues to prompt.
+            Once a valid input is receieved, it is passed to the check_guess function.        
+        
+        '''
         guess = input("Enter a letter: ").lower()
         while True:
             if guess.isalpha() == False and len(guess) !=1:
